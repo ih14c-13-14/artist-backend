1ec40e03fe6ac5adae3c4cf83aa76aa7ffd9f926
ryota658 <ryota05142001@outlook.jp>
2023-07-05 03:32:24 +0000

    issue idをcommentに追加

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	// TODO: 400の判定を追加する
+	// TODO: 400の判定を追加する #90
 	await app.listen(3000);
 }
 bootstrap();
