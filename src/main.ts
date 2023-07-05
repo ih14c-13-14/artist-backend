28b42e06af007523cd68e727a9fa88361f3c3d5c
ryota658 <ryota05142001@outlook.jp>
2023-07-05 03:31:02 +0000

    400commentの追加

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	// TODO: 400の判定を追加する
 	await app.listen(3000);
 }
 bootstrap();
