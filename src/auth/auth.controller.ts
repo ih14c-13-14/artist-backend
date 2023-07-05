1a49f3836ddd09bb08ea1fe8d479587d7119cad9
ryota658 <ryota05142001@outlook.jp>
2023-07-05 03:34:09 +0000

    add: signup機能

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+	Body,
+	Controller,
+	Get,
+	HttpException,
+	HttpStatus,
+	Post,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Users } from '@prisma/client';
+import { SignUpInput } from './dto/signup.input';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+	constructor(private readonly authService: AuthService) {}
+	@Get()
+	getHello(): string {
+		return 'Hello World!';
+	}
+
+	@Post('signup')
+	async signUp(@Body() signUpInput: SignUpInput): Promise<Users> {
+		const user = await this.authService.getUser(signUpInput.email);
+		if (user)
+			throw new HttpException(
+				{
+					message: 'このメールアドレスはすでに登録されています',
+				},
+				HttpStatus.CONFLICT,
+			);
+
+		const newUser = await this.authService.signUp(signUpInput);
+
+		if (!newUser)
+			throw new HttpException(
+				{
+					message: 'ユーザー登録に失敗しました',
+				},
+				HttpStatus.BAD_REQUEST,
+			);
+
+		return newUser;
+	}
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,47 @@
+import { PrismaService } from '@/prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
+import { Users } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+import { uuidv7 } from '@kripod/uuidv7';
+import { convertAgeGroupToNumber } from '@/auth/utils/convertAge';
+import { convertGenderToNumber } from '@/auth/utils/convertGender';
+import { SignUpInput } from './dto/signup.input';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	constructor(private readonly prismaService: PrismaService) {}
+
+	async signUp(signUpInput: SignUpInput): Promise<Users> {
+		const { email, password, age_group, gender, prefecture } = signUpInput;
+
+		const salt = await bcrypt.genSalt();
+		const hashPassword = await bcrypt.hash(password, salt);
+
+		const selectedAgeGroup: number = convertAgeGroupToNumber(age_group);
+		const selectedGender: number = convertGenderToNumber(gender);
+
+		const prefectureId = await this.prismaService.prefectures.findFirst({
+			where: { name: prefecture },
+		});
+
+		// TODO: prefectureIdが見つからなかった場合の例外処理を書く
+		if (!prefectureId) throw new Error('都道府県コードが見つかりません');
+
+		return await this.prismaService.users.create({
+			data: {
+				id: uuidv7(),
+				email,
+				password: hashPassword,
+				age_group: selectedAgeGroup,
+				gender: selectedGender,
+				prefecture: { connect: { id: prefectureId.id } },
+			},
+		});
+	}
+
+	async getUser(email: string): Promise<Users> {
+		return await this.prismaService.users.findUnique({
+			where: { email },
+		});
+	}
+}
