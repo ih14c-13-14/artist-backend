93ab3c39ee1ff495d748f48bfe133fd087cf4e9b
ryota658 <ryota05142001@outlook.jp>
2023-07-06 03:42:43 +0000

    statusCodeを返さないように

---
+++ src/filter/http-exception.filter.ts
@@ -0,0 +1,78 @@
+import {
+	ArgumentsHost,
+	BadRequestException,
+	Catch,
+	ExceptionFilter,
+	ForbiddenException,
+	HttpException,
+	HttpStatus,
+	InternalServerErrorException,
+	NotFoundException,
+	UnauthorizedException,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { components } from '@/generated/schema';
+
+type ErrorResponse = {
+	readonly type?: string;
+	readonly message: ValidationError[];
+};
+
+type ValidationError = {
+	property: string;
+	message: string;
+};
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+	catch(exception: HttpException, host: ArgumentsHost) {
+		const ctx = host.switchToHttp();
+		const response = ctx.getResponse<Response>();
+		const status = exception.getStatus();
+
+		const getResponse = (exception: HttpException, status: number) => {
+			switch (status) {
+				case HttpStatus.BAD_REQUEST:
+					const errorResponse = exception.getResponse() as ErrorResponse;
+					const { type, message } = errorResponse;
+					return {
+						type,
+						message,
+					} satisfies components['schemas']['BadRequestResponse'];
+				case HttpStatus.UNAUTHORIZED:
+					return {
+						message: exception.message,
+					} satisfies components['schemas']['UnauthorizedResponse'];
+				case HttpStatus.FORBIDDEN:
+					return {
+						message: exception.message,
+					} satisfies components['schemas']['ForbiddenResponse'];
+				case HttpStatus.NOT_FOUND:
+					return {
+						message: exception.message,
+					} satisfies components['schemas']['NotFoundResponse'];
+				case HttpStatus.INTERNAL_SERVER_ERROR:
+					return {
+						message: exception.message,
+					} satisfies components['schemas']['InternalServerErrorResponse'];
+			}
+		};
+
+		const handleException = (exception: HttpException) => {
+			const errorResponse = getResponse(exception, status);
+			response.status(status).json(errorResponse);
+		};
+
+		if (exception instanceof BadRequestException) {
+			handleException(exception);
+		} else if (exception instanceof UnauthorizedException) {
+			handleException(exception);
+		} else if (exception instanceof ForbiddenException) {
+			handleException(exception);
+		} else if (exception instanceof NotFoundException) {
+			handleException(exception);
+		} else if (exception instanceof InternalServerErrorException) {
+			handleException(exception);
+		}
+	}
+}
