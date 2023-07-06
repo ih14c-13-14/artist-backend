901f0a4b6edd298612774bdec7f4f86331ed4df6
ryota658 <ryota05142001@outlook.jp>
2023-07-06 14:01:06 +0000

    add: 作者の作品ソートして取得機能

--- src/arts/arts.controller.ts
+++ src/arts/arts.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { ArtsService } from './arts.service';
 import { ArtDTO } from './dto/art-dto';
-
+import { paths } from '@/generated/schema';
 @Controller('arts')
 export class ArtsController {
 	constructor(private readonly artsService: ArtsService) {}
@@ -17,5 +17,22 @@ export class ArtsController {
 			'authors.name': art.authors.name,
 		}));
 		return result;
+	}
+
+	/**
+	 * 作者ソート取得処理
+	 * @throws paths['/api/v1/arts/author']['get']['responses']['200']
+	 */
+	@Get('author')
+	async getAscByAuthor(): Promise<ArtDTO[]> {
+		const arts = await this.artsService.getAscByAuthor();
+		const result: ArtDTO[] = arts.map((art) => ({
+			'arts.id': art.id,
+			'arts.name': art.name,
+			'arts.address': art.address,
+			'arts.image_path': art.image_path,
+			'authors.name': art.authors.name,
+		}));
+		return result satisfies paths['/api/v1/arts/author']['get']['responses']['200']['content']['application/json'];
 	}
 }
