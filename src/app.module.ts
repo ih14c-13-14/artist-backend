35889db517ce888515bbb9752ca3743ce93eafbb
4s2r9 <nagarebosi429@gmail.com>
2023-07-06 07:20:16 +0000

    add:ログイン時パスワードを忘れた時の機能
    Co-authored-by: A.Yamamoto <na2na-p@users.noreply.github.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArtsModule } from './arts/arts.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-	imports: [ArtsModule],
+	imports: [PrismaModule, ArtsModule, UsersModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
