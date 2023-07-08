7ed187b67f35ddda7d3b10a3ddf5d8905050604a
ryota658 <ryota05142001@outlook.jp>
2023-07-08 14:46:04 +0000

    add: 作品の詳細情報取得

--- src/arts/arts.service.ts
+++ src/arts/arts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { ArtServiceResponse } from './dto/art-service-response';
+import { artDetailServiceResponse } from './dto/art-detail-service-response';
 
 @Injectable()
 export class ArtsService {
@@ -39,6 +40,40 @@ export class ArtsService {
 				authors: {
 					name_kana: 'asc',
 				},
+			},
+		});
+	}
+
+	async getArtDetail(art_id: string): Promise<artDetailServiceResponse> {
+		return await this.prismaService.arts.findUnique({
+			select: {
+				id: true,
+				name: true,
+				address: true,
+				image_path: true,
+				description: true,
+				is_public: true,
+				created_year: true,
+				datetime_description: true,
+				closed_day_description: true,
+				arts_institutions: {
+					select: {
+						institution: {
+							select: {
+								admission_fee_description: true,
+							},
+						},
+					},
+				},
+				authors: {
+					select: {
+						name: true,
+						image_path: true,
+					},
+				},
+			},
+			where: {
+				id: art_id,
 			},
 		});
 	}
---
+++ src/arts/dto/art-detail.d.ts
@@ -0,0 +1,14 @@
+export type ArtDetailDTO = {
+	'arts.id': string;
+	'arts.name': string;
+	'arts.address': string;
+	'arts.image_path': string[];
+	'arts.description': string;
+	'arts.is_public': boolean;
+	'arts.created_year': number;
+	'arts.datetime_description': string;
+	'arts.closed_day_description': string;
+	'institutions.admission_fee_description': string;
+	'authors.name': string;
+	'authors.image_path': string;
+};
