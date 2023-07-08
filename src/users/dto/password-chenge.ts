e9d4ebb777600dc4fb32aa400fb7a8c46f68b23b
4s2r9 <nagarebosi429@gmail.com>
2023-07-08 14:13:43 +0000

    fix : 修正

--- src/users/dto/password-chenge.ts
+++ src/users/dto/password-chenge.ts
@@ -1,6 +1,13 @@
-import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+import {
+	IsNotEmpty,
+	IsString,
+	Matches,
+	MaxLength,
+	MinLength,
+} from 'class-validator';
 
 export class PasswordChange {
+	@IsNotEmpty()
 	@IsString()
 	@MinLength(8, {
 		message: '8文字以上である必要があります。',
