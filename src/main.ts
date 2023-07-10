b63df85838c02c5ef36e86421695050168c33dd5
ryota658 <ryota05142001@outlook.jp>
2023-07-10 13:40:53 +0900

    fix: lintを通るように

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
 			});
 		},
 	}),
-	app.useGlobalFilters(new HttpExceptionFilter());
+		app.useGlobalFilters(new HttpExceptionFilter());
 
 	await app.listen(3000);
 }
