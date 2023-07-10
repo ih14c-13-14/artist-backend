9173536a75841560394910bfb5ac5bc6229276cf
ryota658 <ryota05142001@outlook.jp>
2023-07-10 18:27:11 +0900

    uuidPipeの修正

--- src/main.ts
+++ src/main.ts
@@ -33,8 +33,8 @@ async function bootstrap() {
 				],
 			});
 		},
-	}),
-		app.useGlobalFilters(new HttpExceptionFilter());
+	});
+	app.useGlobalFilters(new HttpExceptionFilter());
 
 	await app.listen(3000);
 }
