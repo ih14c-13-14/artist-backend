4f382a06636a045d9e3fbf6e53eba623be7fe7e2
Issiki0416 <siroha323@gmail.com>
2023-07-11 07:02:01 +0000

    add: map機能

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { ArtsModule } from './arts/arts.module';
 import { UsersModule } from './users/users.module';
+import { MapModule } from './map/map.module';
 
 @Module({
-	imports: [PrismaModule, AuthModule, ArtsModule, UsersModule],
+	imports: [PrismaModule, AuthModule, ArtsModule, UsersModule, MapModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
